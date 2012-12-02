93f3d9afdd0e30bf1719473dab31cc55a4abfcfc
Andrew Liu <rng1@ymail.com>
2012-12-02 02:57:45 -0800

    added cookies

---
+++ play_app/public/javascripts/authenticate.js
@@ -0,0 +1,75 @@
+// Checks if already logged in by checking if valid session cookie exists.
+function isLoggedIn() {
+	if ($.cookie('account') != null) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+// If not already logged in, attempts to parse access_token from website url.
+function login() {
+	if (isLoggedIn()) {
+		access_token = $.cookie('account');
+	}
+
+	else {
+		urlname = document.location.href.toString();
+		if (urlname.search("&access_token") != -1) {
+			suburl = urlname.split("&access_token=")[1];
+			access_token = suburl.split("&refresh_token=")[0];
+		}
+		else {
+			return;
+		}
+	}
+
+	return access_token;
+}
+
+
+// Accesses database from Singly and imports to Firebase.
+function importToFirebase() {
+	ACCESS_TOKEN = login();
+	request_url = "https://api.singly.com/profile?access_token=" + ACCESS_TOKEN
+
+	if (ACCESS_TOKEN) {
+		$.ajax({
+
+		    url: request_url,
+		    
+		    dataType: "json",
+		    
+		    success: function (d) {
+		    
+		      addUserToFirebase(d);
+		        
+		    },
+
+		    error: function () {
+		        console.log("ERROR: Unable to fetch user data.");
+		    }
+
+		});
+	}
+
+	if (isLoggedIn()) {
+		return;
+	}
+	else {
+		$.cookie('account', ACCESS_TOKEN);
+	}
+
+}
+
+function addUserToFirebase(user) {
+	myDataRef.child('users').child(user.id).set(user);
+}
+
+function initializelogin() {
+	isLoggedIn();
+	login();
+	importToFirebase();
+}
